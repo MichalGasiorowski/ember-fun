b0c2edb5f1ae2a2b1d551f23f5db4fb11a688f7f
MGasiorowski <enkidupal@gmail.com>
2014-06-20 23:29:50 +0200

    Displaying the Number of Incomplete Todos finished

---
+++ TodoMVC/js/controllers/todo_controller.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Michal on 6/20/2014.
+ */
+Todos.TodoController = Ember.ObjectController.extend({
+  isCompleted: function(key, value){
+    var model = this.get('model');
+
+    if(value === undefined) {
+      // property being used as a getter
+      return model.get('isCompleted');
+    } else {
+      // property being used as a setter
+      model.set('isCompleted', value);
+      model.save();
+      return value;
+    }
+  }.property('model.isCompleted')
+});
